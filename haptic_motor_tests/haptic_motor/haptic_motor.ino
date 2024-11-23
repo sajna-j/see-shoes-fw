cf91ac6596d71bc0c612ed9cc6ccbfb1684ff8ae
sajna-j <sajna.j@northeastern.edu>
2024-11-23 09:21:50 -0500

    analog write at various inputs

--- haptic_motor_tests/haptic_motor/haptic_motor.ino
+++ haptic_motor_tests/haptic_motor/haptic_motor.ino
@@ -1,18 +1,38 @@
-// const int motorPin = D5;  // Use a PWM-capable pin (e.g., 3, 5, 6, 9, 10, or 11)
+const int frontMotor = A7;
+float userNum = 255; // To store the user input
+
 
 void setup() {
-  pinMode(D5, OUTPUT);  // Set the motor pin as an output
+  pinMode(frontMotor, OUTPUT);
   Serial.begin(9600);  // Initialize serial communication for debugging
 }
 
 void loop() {
+  
+  if (Serial.available() > 0) {
+    // Read the user input
+    userNum = Serial.parseFloat(); // Read the integer input
+
+    // Clear the Serial buffer to discard trailing characters
+    while (Serial.available() > 0) {
+      Serial.read();
+    }
+
+    Serial.print("You entered: ");
+    Serial.println(userNum); // Echo the input back to the Serial Monitor
+  }
+
+  Serial.print("User num is: ");
+  Serial.println(userNum); // Echo the input back to the Serial Monitor
+
   // Turn on the motor
-  digitalWrite(D5, 255);  // vibrate
-  Serial.println("Motor ON");
+  analogWrite(frontMotor, userNum);  // vibrate
+  Serial.print("Front Motor ON ");
   delay(3000);  // Run for 3 second
 
   // Turn off the motor
-  digitalWrite(D5, 0);  // Turn off
-  Serial.println("Motor OFF!");
+  analogWrite(frontMotor, 0);  // Turn off
+  Serial.println("Front Motor OFF!");
   delay(3000);  // Stay off for 3 sec
+
 }
